5bf6c9ffe02a4d6f302d40c9056788b21e7eecde
Luiz Henrique <luiz.ventura01@etec.sp.gov.br>
2025-11-24 15:09:45 -0300

    Update 0.1 - Adição do Forms e estilização

---
+++ Admin-Floresol - Sistema/Floresol-ADM/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Floresol_ADM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Cadastrar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Floresol_ADM.Properties.Resources.logo_3;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(313, 62);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(188, 175);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Cadastrar
+            // 
+            this.Cadastrar.BackgroundImage = global::Floresol_ADM.Properties.Resources.fundo_2;
+            this.Cadastrar.ForeColor = System.Drawing.Color.White;
+            this.Cadastrar.ImageKey = "(nenhum/a)";
+            this.Cadastrar.Location = new System.Drawing.Point(313, 318);
+            this.Cadastrar.Name = "Cadastrar";
+            this.Cadastrar.Size = new System.Drawing.Size(181, 29);
+            this.Cadastrar.TabIndex = 1;
+            this.Cadastrar.Text = "Cadastrar";
+            this.Cadastrar.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Floresol_ADM.Properties.Resources.logo;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(313, 266);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Entrada ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseWaitCursor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Cadastrar);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.DarkGray;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Cadastrar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
