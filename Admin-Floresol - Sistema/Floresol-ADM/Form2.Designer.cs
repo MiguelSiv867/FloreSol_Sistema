d43d8896963834792610b0d66521df61c070deaf
Gustavo11032009 <gustavo.savazzi@etec.sp.gov.br>
2025-11-25 17:47:32 -0300

    Update Form2.Designer.cs

--- Admin-Floresol - Sistema/Floresol-ADM/Form2.Designer.cs
+++ Admin-Floresol - Sistema/Floresol-ADM/Form2.Designer.cs
@@ -107,7 +107,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.Graphicsunit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.label3.Location = new System.Drawing.Point(518, 43);
             this.label3.Name = "label3";
